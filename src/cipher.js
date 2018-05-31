0977a6d2542f53467764c3cd81168620ca80b151
AnaeyGuillen <anaey_gu@hotmail.com>
2018-05-31 12:59:09 -0500

    Cifrado y descifrado completo

--- src/cipher.js
+++ src/cipher.js
@@ -4,8 +4,9 @@ window.cipher = {
     let mensajeCodificado = '';
     for (let index = 0; index < string.length; index++) {
       let codigoCaracter = string.toUpperCase().charCodeAt(index);
-      if(codigoCaracter >= 32 && codigoCaracter <= 64){
-         
+      if (codigoCaracter == 32){
+        let space = " "; 
+        mensajeCodificado = mensajeCodificado + space;
       } else {
         let nuevoCaracter = (codigoCaracter - 65 + offset)%26 + 65;
         let nuevoCaracterCodificado = String.fromCharCode(nuevoCaracter);
@@ -18,8 +19,8 @@ window.cipher = {
   decode: (offset, string) => {
     let mensajeDecodificado='';
     for (let index=0; index<string.length; index++){
-      let codigoCaracter=string.toUpperCase().charCodeAt(index);
-      let nuevoCaracter= (codigoCaracter+65-offset)%26+65;
+      let codigoCaracter = string.toUpperCase().charCodeAt(index);
+      let nuevoCaracter = (codigoCaracter+65-offset)%26+65;
       let nuevoCaracterDecodificado = String.fromCharCode(nuevoCaracter);
       mensajeDecodificado = mensajeDecodificado + nuevoCaracterDecodificado;
 
